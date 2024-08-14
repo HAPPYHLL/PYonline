baa40edb6787eb036f9b916962dce45ab8b1963a
HAPPYHLL <1783684807@qq.com>
2024-08-14 11:13:15 +0800

    update

---
+++ #临时使用up.py
@@ -0,0 +1,13 @@
+#面向对象
+# coding=utf-8
+name = ['张三', '李四', '王五', '赵六', '前妻',]
+# while True:
+    # name.append(input("请输入姓名(q退出)："))
+    # if name[-1] == "q":
+        
+    #     name.pop()
+    #     for i in name:
+    #         print(i)
+    #     break
+name.pop()
+print(name)  
