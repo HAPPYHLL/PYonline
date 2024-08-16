b7d75835bf6dcb47351528933d81acf7440706b1
帝君紫魈 <1783684807@qq.com>
2024-08-16 10:15:03 +0800

    更新 #临时使用up.py

--- #临时使用up.py
+++ #临时使用up.py
@@ -1,13 +1,5 @@
 #面向对象
 # coding=utf-8
-name = ['张三', '李四', '王五', '赵六', '前妻',]
-# while True:
-    # name.append(input("请输入姓名(q退出)："))
-    # if name[-1] == "q":
-        
-    #     name.pop()
-    #     for i in name:
-    #         print(i)
-    #     break
-name.pop()
-print(name)   
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['color'])
+print(alien_0['points'])
